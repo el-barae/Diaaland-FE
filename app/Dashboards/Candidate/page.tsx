70b9a28404f21b5a457c8ffb1ec7ac3ccbb5d476
el-barae <elbarae.akachar@gmail.com>
2024-01-17 18:24:00 +0100

    Candidate buttons menu

--- app/Dashboards/Candidate/page.tsx
+++ app/Dashboards/Candidate/page.tsx
@@ -3,13 +3,20 @@ import Image from 'next/image'
 import { useState ,useEffect} from "react"
 import axios from 'axios'
 import React from 'react';
+import { useRouter } from 'next/navigation';
 import Cookies from 'js-cookie';
 import './style.scss';
 import {ThemeProvider} from 'next-themes'
 import Navbar from '@/components/HomePage/Navbar/Navbar'
 import Jobs from '@/components/Candidate/Jobs/Jobs'
 
+const router = useRouter();
 
+const toJobs = () =>{
+  
+}
+
+let x = 1;
 
 const Profile = () => {
     return (
@@ -20,29 +27,13 @@ const Profile = () => {
                 <h1>Candidate</h1>
               </div>
               <div className='content'>
-              <div className='Menu'>
-              <li>
-                <ul>profile</ul>
-                <ul>settings</ul>
-                <ul></ul>
-              </li>
+                <div className='Menu'>
+                  <button>Profile</button>
+                  <button>My jobs</button>
+                  <button>My favoris</button>
               </div>
               <div className='Candidate-box'>
                 <Jobs/>
-                <h1>My favorites:</h1>
-                <div className='favories'>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates laborum ex velit doloremque officia. Commodi doloremque inventore magnam tempora exercitationem earum blanditiis. Optio earum aliquam soluta minima velit. Reiciendis, aperiam?
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, suscipit id harum sit, vero tenetur deleniti perferendis odio nobis consequatur quae nisi, debitis molestiae! Eius aliquid impedit incidunt aspernatur ut?
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam animi perspiciatis, eos est reiciendis deserunt neque explicabo nisi rem, saepe iste nihil maiores praesentium quidem at. Facere repudiandae architecto alias.
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla accusantium distinctio consequuntur magni, facere similique suscipit rerum excepturi doloribus omnis error illum placeat optio laboriosam eligendi et qui eum modi.
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quas autem quidem cum impedit quia vero vitae! Itaque, sunt voluptate, corporis maiores fugit fuga odit, veniam provident esse non ab!
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam nesciunt quae distinctio laborum, molestiae consectetur at temporibus, exercitationem voluptatibus blanditiis, a praesentium. Eaque consectetur atque inventore ipsum et aut sint!
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi tenetur aut debitis consequuntur. Iste quasi architecto officiis doloremque dolorum reiciendis commodi cupiditate in esse deleniti inventore, tenetur, ab, aut praesentium!
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae sequi cupiditate laudantium, illum consequatur dicta alias voluptatum repellat omnis fugit necessitatibus assumenda cum! Nesciunt ea quaerat corporis! Numquam, officia deserunt!
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero eveniet numquam totam quia, vero a neque. Exercitationem unde accusantium aliquam sapiente ad, enim eligendi animi, nesciunt, tempore incidunt ut aliquid.
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, temporibus saepe porro eveniet fugiat officia in ea maxime iure, quis et. Expedita repellendus iure sint. Adipisci modi odit beatae velit!
-                  </p>
-                </div>
               </div>
               </div>
             </div>
--- components/Candidate/Favoris/Favoris.tsx
+++ components/Candidate/Favoris/Favoris.tsx
@@ -1,6 +1,64 @@
 import './favoris.scss'
 
-const Favoris = () =>{
+import { useState ,useEffect} from "react"
+import axios from 'axios'
+import React from 'react';
+import Cookies from 'js-cookie';
 
+interface Job {
+    id: number;
+    name: string;
+    description: string;
+    numberOfPositions: number;
+    closeDate: string;
+  }
+
+  interface RepeatClassNTimesProps {
+    className: string;
+    n: number;
+    jobsData: Job[];
+  }
+
+  const RepeatClassNTimes: React.FC<RepeatClassNTimesProps> = ({ className, n, jobsData }) => {
+      return(
+        <>
+        {jobsData.map((job) => (
+          <div key={job.id} className={className}>
+          <h1>{job.name} :</h1>
+          <p>
+            Description: {job.description} <br/> Number of positions: {job.numberOfPositions} 
+          </p>
+          <p>Close Date: {job.closeDate}</p>
+        </div>
+        ))}
+        </>
+      )
+    }
+
+const Favoris = () =>{ 
+    const [jobsData, setJobsData] = useState<Job[]>([]);
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            Cookies.set("id","1")
+            const id = Cookies.get("id");
+            const response = await axios.get('http://localhost:7777/api/v1/candidate-jobs/byCandidate/'+String(id));         
+            setJobsData(response.data);
+          } catch (error) {
+            console.error('Erreur lors de la récupération des données :', error);
+          }
+        };
+        fetchData();
+  }, []);
+  return(
+    <>
+    <div className='jobs'>
+        <h1>My favoris:</h1>
+                <div className='lists'>
+                  <RepeatClassNTimes className="list" n={jobsData.length} jobsData={jobsData} />
+                </div>
+    </div>
+    </>
+  );
 }
 export default Favoris;
