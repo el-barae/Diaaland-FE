98c0080b3c84494b4f48eac9f42f38aea4feb09a
el-barae <elbarae.akachar@gmail.com>
2024-01-23 12:19:39 +0100

    btn delete project

--- components/Candidate/Projects/Projects.tsx
+++ components/Candidate/Projects/Projects.tsx
@@ -3,6 +3,7 @@ import './Projects.scss';
 import { useState,useEffect } from 'react';
 import Cookies from 'js-cookie';
 import axios from 'axios';
+import { describe } from 'node:test';
 
 interface project{
     id: number;
@@ -18,6 +19,60 @@ interface RepeatClassNTimesProps {
     projectsData: project[];
   }
 
+const Projects = () => {
+  const [name,setName] = useState('')
+  const [startDate,setStartDate] = useState('')
+  const [endDate,setEndDate] = useState('')
+  const [desc,setDesc] = useState('')
+  const [candidateId,setCandidateId] = useState(1)
+    const [projectsData,setProjectsData] = useState<project[]>([])
+
+    const handleAddProject = async (e:any)  =>{
+      e.preventDefault()
+      axios.post('http://localhost:7777/api/v1/projects', {
+        "name": name,
+        "startDate": startDate,
+        "endDate": endDate,
+        "description": desc,
+        "candidate": {
+          "id": 1
+        }
+        }/*, {
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          },
+        }*/)
+        .then(function (response) {
+        console.log(response);
+        alert("Your post had been sent to admin ")
+        })
+        .catch(function (error) {
+        alert(error.message);
+        });
+    }
+
+    const handleDelete = async (e:any, id:number) =>{
+      e.preventDefault()
+      axios.delete('http://localhost:7777/api/v1/projects')
+       .catch(function (error) {
+        console.log(error);
+       });
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            Cookies.set("id","1")
+            const id = Cookies.get("id");
+            const response = await axios.get('http://localhost:7777/api/v1/projects/byCandidate/'+String(id));         
+            setProjectsData(response.data);
+          } catch (error) {
+            console.error('Erreur lors de la récupération des données :', error);
+          }
+        };
+        fetchData();
+  }, []);
+
   const RepeatClassNTimes: React.FC<RepeatClassNTimesProps> = ({ className, n, projectsData }) => {
     if(projectsData.length != 0)
       return(
@@ -32,27 +87,13 @@ interface RepeatClassNTimesProps {
             startDate: {project.startDate}
           </p>
           <p>Close Date: {project.endDate}</p>
+          <button onClick={(e) => handleDelete(e, project.id)}>Delete</button>
         </div>
         ))}
         </>
       )
     }
 
-const Projects = () => {
-    const [projectsData,setProjectsData] = useState<project[]>([])
-    useEffect(() => {
-        const fetchData = async () => {
-          try {
-            Cookies.set("id","1")
-            const id = Cookies.get("id");
-            const response = await axios.get('http://localhost:7777/api/v1/projects/byCandidate/'+String(id));         
-            setProjectsData(response.data);
-          } catch (error) {
-            console.error('Erreur lors de la récupération des données :', error);
-          }
-        };
-        fetchData();
-  }, []);
     return(
         <div className="jobs">
             <h1>Projects</h1>
