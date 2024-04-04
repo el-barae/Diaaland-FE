5de693b55d17b5b9cf9bb0e05a46dcd4ded6411f
el-barae <elbarae.akachar@gmail.com>
2024-04-04 13:55:12 +0000

    filtre candidates by skill

--- components/Admin/Candidates/Candidates.tsx
+++ components/Admin/Candidates/Candidates.tsx
@@ -25,6 +25,18 @@ interface Candidate{
 	image:string;
 }
 
+interface skill{
+  id: number;
+  name : string;
+  type : string;
+}
+
+interface RepeatClassNTimesProps1 {
+  className: string;
+  n: number;
+  skillsAll: skill[];
+}
+
   interface RepeatClassNTimesProps {
     className: string;
     n: number;
@@ -65,6 +77,9 @@ const [candidateResume, setCandidateResume] = useState<string>('');
 const [candidateImage, setCandidateImage] = useState<string>('');
 const [modalOpen, setModalOpen] = useState(false);
 
+const [skill,setSkill] = useState('')
+const [skillsAll,setSkillsAll] = useState<skill[]>([])
+
 const handleModify = (id: number, firstName: string, lastName: string, email: string, city: string, country: string, address: string, accountStatus: string, phone: string, jobStatus: string, expectedSalary: number, linkedin: string, github: string, portfolio: string, blog: string, resume: string, image: string) => {
   setCandidateId(id);
   setCandidateFirstName(firstName);
@@ -85,6 +100,33 @@ const handleModify = (id: number, firstName: string, lastName: string, email: st
   setCandidateImage(image);
   setModalOpen(true);
 };
+
+const getSkillIdByName = (skillName: string): number | null => {
+  const foundSkill = skillsAll.find(skill => skill.name === skillName);
+  return foundSkill ? foundSkill.id : null;
+};
+
+const handleFiltre = async () =>{
+  const ID = getSkillIdByName(skill);
+  if (ID !== null) {
+    try {
+      const response = await axios.get(API_URL+'/api/v1/candidate-skills/bySkill/' + String(ID), {
+        headers: {
+          'Authorization': 'Bearer ' + token
+        }
+      });
+      const candidateSkills: Candidate[] = response.data;
+      setCandidatesData(candidateSkills);
+      console.log('Candidates filtred');
+    }catch(error){
+  
+    }
+  } else {
+    console.log('candidates not found');
+    console.log(skill)
+  }
+  
+}
 
 
 const RepeatClassNTimes: React.FC<RepeatClassNTimesProps> = ({ className, n, candidatesData }) => {
@@ -114,7 +156,13 @@ const RepeatClassNTimes: React.FC<RepeatClassNTimesProps> = ({ className, n, can
                 'Authorization': 'Bearer ' + token
               }
             });   
-                  
+            const res = await axios.get(API_URL+'/api/v1/skills', {
+              headers: {
+                'Authorization': 'Bearer ' + token
+              }
+            });
+            const allSkills: skill[] = res.data;
+            setSkillsAll(allSkills);
             setCandidatesData(response.data);
           } catch (error) {
             console.error('Erreur lors de la récupération des données :', error);
@@ -122,6 +170,18 @@ const RepeatClassNTimes: React.FC<RepeatClassNTimesProps> = ({ className, n, can
         };
         fetchData();
   }, []);
+
+  const RepeatClassNTimes1: React.FC<RepeatClassNTimesProps1> = ({ className, n, skillsAll }) => {
+    if(skillsAll.length != 0)
+      return(
+        <>
+        {skillsAll.map((skill) => (
+          <option key={skill.name} value={skill.name} >{skill.name}</option>
+        ))}
+        </>
+      )
+    }
+
     return(
     <div className="jobs">
         <div className='head'>
@@ -130,6 +190,13 @@ const RepeatClassNTimes: React.FC<RepeatClassNTimesProps> = ({ className, n, can
           <span className="button__text">Add</span>
           <span className="button__icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 24 24" stroke-width="2" stroke-linejoin="round" stroke-linecap="round" stroke="currentColor" height="24" fill="none" className="svg"><line y2="19" y1="5" x2="12" x1="12"></line><line y2="12" y1="12" x2="19" x1="5"></line></svg></span>
           </button>
+        </div>
+        <div className="filtre">
+          <h2>filter by Skills</h2>
+          <select id="skills" name="skills" value={skill} onChange={(e) => setSkill(e.target.value)}>
+                    <RepeatClassNTimes1 className="list" n={skillsAll.length} skillsAll={skillsAll} />
+                </select>
+          <button onClick={(e) => handleFiltre()}>Filtre</button>
         </div>
         <div className='lists'>
                   <RepeatClassNTimes className="list" n={candidatesData.length} candidatesData={candidatesData} />
